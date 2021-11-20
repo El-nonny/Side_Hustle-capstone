a5b3318bfa0746f84320eb2452edaad6edf58f43
El-nonny <85935192+El-nonny@users.noreply.github.com>
2021-11-20 15:48:04 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:login_ui/layOutScreen/loginscreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
