e028716acf6e43f9f4815d3b0f22e1923a00805d
El-nonny <85935192+El-nonny@users.noreply.github.com>
2021-11-30 15:19:53 +0100

    Delete lib directory

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
-import 'package:login_ui/layOutScreen/loginscreen.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
